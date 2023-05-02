5737bb7c296dc12224dca67c03f2071316baa9ca
Syyd007 <sayedsoudana054@gmail.com>
2023-05-02 10:33:21 +0100

    first commit

---
+++ src/componnent/FullName.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function FullName() {
+    return (
+        <div>
+           name: sayed
+        </div>
+    )
+}
+export default FullName
---
+++ src/componnent/ProfilPhoto.js
@@ -0,0 +1,6 @@
+import img from "../logo.svg"
+
+
+function ProfileApp () {
+    return ( <img alt="" src="https://www.tunisianet.com.tn/65532-large/pc-portable-asus-rog-gl553vw-i7-6e-gen-16-go.jpg" />)}
+export default ProfileApp;
